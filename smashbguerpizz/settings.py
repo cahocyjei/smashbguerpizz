5efc7703f8a28758003b838ee68c41a6ce066eb1
Omar Cabarcas <cahocyjei@gmail.com>
2024-11-20 11:26:39 -0500

    STARTUP [Configurando Postgresql, migration]

--- smashbguerpizz/settings.py
+++ smashbguerpizz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^gflhj#=$w=fpws7t%&65h-nzg8g+ttc3i@_7ewl=e(@d^%@v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'smashbguerpizz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smashBP',
+        'USER': 'troyka',
+        'PASSWORD': '1914',
+        'HOST': 'dbSmash',
+        'PORT': '5432'
     }
 }
 
